c746f2007b901735f1d2ed3c72d167c99117f5c9
LQuesadaM <luisquesada163@gmail.com>
2024-03-26 16:37:14 -0300

    🍻 sum all the numbers in the array

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const getMean = (array) => {
+  const sum = array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+};
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
