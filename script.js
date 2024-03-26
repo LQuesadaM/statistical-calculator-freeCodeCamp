0235882c124707c27f85300ff2b103bd50d4f0d8
LQuesadaM <luisquesada163@gmail.com>
2024-03-26 19:30:00 -0300

    🍻 obtaining the return value of the median and displaying on the screen

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const getMean = (array) =>
   array.reduce((accumulator, currentValue) => accumulator + currentValue, 0) /
   array.length;
 
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
+  return median;
+};
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
@@ -9,5 +18,8 @@ const calculate = () => {
     .map((element) => Number(element))
     .filter((element) => !isNaN(element));
   const mean = getMean(numbers);
+  const median = getMedian(numbers);
+
   document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
 };
