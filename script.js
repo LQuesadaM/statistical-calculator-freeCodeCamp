6cbdbf6240eb3c60abf140af7b76965b581482fa
LQuesadaM <luisquesada163@gmail.com>
2024-03-27 09:49:41 -0300

    🍻 calculate the square root and returns the value

--- script.js
+++ script.js
@@ -41,6 +41,8 @@ const getVariance = (array) => {
 
 const getStandardDeviation = (array) => {
   const variance = getVariance(array);
+  const standardDeviation = Math.sqrt(variance);
+  return standardDeviation;
 };
 
 const calculate = () => {
